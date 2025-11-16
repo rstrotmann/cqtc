4ae2b7611dd54a2eb7a44d5473b589f744f9051e
rstrotmann <rstrotmann@users.noreply.github.com>
2025-11-16 20:27:39 +0100

    add ACTIVE field for demo data sets

--- data-raw/make_cqtc_data.R
+++ data-raw/make_cqtc_data.R
@@ -6,7 +6,7 @@ dofetilide_cqtc <- dof %>%
   mutate(
     QTCF = QTcF, DQTCF = QTcF.CFB, QT = QTm, RR = RRm, CONC = CONC,
     NTIME = TIME) %>%
-  select(ID, NTIME, CONC, QT, QTCF, DQTCF, RR) %>%
+  select(ID, ACTIVE, NTIME, CONC, QT, QTCF, DQTCF, RR) %>%
   new_cqtc() %>%
   mutate(BL_QTCF = QTCF[NTIME == -0.5], .by = "ID")
 
@@ -15,7 +15,7 @@ verapamil_cqtc <- ver %>%
   mutate(
     QTCF = QTcF, DQTCF = QTcF.CFB, QT = QTm, RR = RRm, CONC = CONC,
     NTIME = TIME) %>%
-  select(ID, NTIME, CONC, QT, QTCF, DQTCF, RR) %>%
+  select(ID, ACTIVE, NTIME, CONC, QT, QTCF, DQTCF, RR) %>%
   new_cqtc() %>%
   mutate(BL_QTCF = QTCF[NTIME == -0.5], .by = "ID")
 
