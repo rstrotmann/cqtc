2e92fea31de6651445cbf48c945196be1b2740be
rstrotmann <rstrotmann@users.noreply.github.com>
2026-01-28 21:09:30 +0100

    add SCR_002_Clinical_Data data set

---
+++ data-raw/make_SCR-002_Clinical_Data.R
@@ -0,0 +1,13 @@
+# source: https://physionet.org/content/ecgrdvq/1.0.0/#files-panel
+
+#' Johannesen L, Vicente J, Mason JW, Sanabria C, Waite-Labott K, Hong M, Guo P,
+#' Lin J, Sørensen JS, Galeotti L, Florian J, Ugander M, Stockbridge N, Strauss
+#' DG. Differentiating Drug-Induced Multichannel Block on the Electrocardiogram:
+#' Randomized Study of Dofetilide, Quinidine, Ranolazine, and Verapamil. Clin
+#' Pharmacol Ther. 2014 Jul 23. doi: 10.1038/clpt.2014.155.
+
+SCR_002_Clinical_Data <- data.table::fread(
+  "https://physionet.org/files/ecgrdvq/1.0.0/SCR-002.Clinical.Data.csv?download"
+)
+
+usethis::use_data(SCR_002_Clinical_Data, overwrite = TRUE)
